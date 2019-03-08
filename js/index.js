8b3b405510b1ea77bde727f214454349ce9bc053
slavik <slavik.fedcyshyn@gmail.com>
2019-03-08 12:16:38 +0000

    calc

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function insert(num){
+    document.form.textview.value = document.form.textview.value+num;
+}
+
+function equal(){
+    const exp = document.form.textview.value;
+    if(exp){
+        document.form.textview.value = eval(exp);
+    }
+}
+function clean(){
+    document.form.textview.value= "";
+}
+function back(){
+    const exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0,exp.length-1);
+}
